978ee5aaecd8d742a16afc1c6f19776d7591fcf0
Veronika <veronika.a.kemp@gmail.com>
2025-01-25 15:23:30 +0000

    step 6 commit addition

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,51 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import { rules, getQuestionandAnswer } from '../src/games/calc.js';
+import gameLogic from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName} !`);
-
-console.log('What is the result of the expression?');
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-for (let i = 0; i < 3; i += 1) {
-  const number1 = getRandomInt(50);
-  const number2 = getRandomInt(50);
-  const operator = ['+', '-', '*'];
-  const randomIndex = getRandomInt(2);
-  const randomOperator = operator[randomIndex];
-  console.log(`Question: ${number1} ${randomOperator} ${number2}`);
-
-  let result;
-  switch (randomOperator) {
-    case '+':
-      result = number1 + number2;
-      break;
-    case '-':
-      result = number1 - number2;
-      break;
-    default:
-      result = number1 * number2;
-      break;
-  }
-
-  const check = result.toString();
-
-  const userAnswer = readlineSync.question('Your answer: ');
-
-  if (check === userAnswer) {
-    console.log('Correct');
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result}'.
-Let's try again, ${userName}!`);
-    break;
-  }
-  if (i === 2) {
-    console.log(`Congratulations, ${userName}!`);
-  }
-}
+gameLogic(rules, getQuestionandAnswer);
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,42 +1,6 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import { rules, getQuestionandAnswer } from '../src/games/even.js';
+import gameLogic from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName} !`);
-
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-for (let i = 0; i < 3; i += 1) {
-  const randomNumber = getRandomInt(100);
-  console.log(`Question: ${randomNumber}`);
-
-  const userAnswer = readlineSync.question('Your answer: ');
-
-  const check = randomNumber % 2;
-  let rightAnswer = '';
-  if (check === 0) {
-    rightAnswer = 'yes';
-  } else {
-    rightAnswer = 'no';
-  }
-
-  const oppositeAnswer = (userAnswer === 'yes' ? 'no' : 'yes');
-
-  if (userAnswer === rightAnswer) {
-    console.log('Correct');
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${oppositeAnswer}'.
-Let's try again, ${userName}!`);
-    break;
-  }
-  if (i === 2) {
-    console.log(`Congratulations, ${userName}!`);
-  }
-}
+gameLogic(rules, getQuestionandAnswer);
