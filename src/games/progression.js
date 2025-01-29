bf22af02b25f41f1c375d6b6ab2861ef8660fbc1
Veronika <veronika.a.kemp@gmail.com>
2025-01-29 09:48:14 +0000

    step 8 edit

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,15 +7,12 @@ function getQuestionandAnswer() {
   const question = [];
   const firstNum = getRandomInt(0, 100);
   question.push(firstNum);
-  console.log(firstNum);
   const progressionStep = getRandomInt(1, 50);
-  console.log(progressionStep);
   let nextNum = firstNum + progressionStep;
   question.push(nextNum);
   for (let i = question.length; i < arrLength; i += 1) {
     nextNum += progressionStep;
     question.push(nextNum);
-    console.log(nextNum);
   }
   const randomIndex = getRandomInt(0, question.length);
   const correctAnswer = question[randomIndex];
